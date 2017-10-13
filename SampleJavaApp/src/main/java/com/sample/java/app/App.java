b69cab909df6fc8a45eff22f84305b6639430d3d
MOKSHAGNA\Ashok <chashokmca@gmail.com>
2017-10-14 00:00:18 +0530

    initial commit

---
+++ SampleJavaApp/src/main/java/com/sample/java/app/App.java
@@ -0,0 +1,21 @@
+package com.sample.java.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+	
+	public int add(int a, int b) {
+		return a+b;
+	}
+	
+	public int sub(int a, int b) {
+		return a-b;
+	}
+}
